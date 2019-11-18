5a1158ddee25eb033c892fad8bfe51e5f77e02e3
swanandm <swanand.mehta94@gmail.com>
2019-11-18 13:33:49 +0530

    Added : + Project settings + eclipse settings + class path + pom.xml
    Removed :
    
    Modified :
    
    Signed-off-by: swanandm <swanand.mehta94@gmail.com>

---
+++ src/main/java/com/keep/config/AppInitializer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.keep.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author swanandm
+ *
+ */
+@SpringBootApplication
+public class AppInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInitializer.class, args);
+	}
+	
+}
