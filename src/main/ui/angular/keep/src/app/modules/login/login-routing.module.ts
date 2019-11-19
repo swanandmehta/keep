b919a239dfd0a0d00aacb0bed5a373b5e7fe87be
swanandm <swanand.mehta94@gmail.com>
2019-11-19 11:13:28 +0530

    Added : + Added reset password component
    Removed :
    
    Modified :
    + Added reset password routing for reset password component
    
    Signed-off-by: swanandm <swanand.mehta94@gmail.com>

--- src/main/ui/angular/keep/src/app/modules/login/login-routing.module.ts
+++ src/main/ui/angular/keep/src/app/modules/login/login-routing.module.ts
@@ -1,12 +1,27 @@
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{
-  path : '',
-  component : LoginComponent
-}];
+const routes: Routes = [
+  {
+    path : '',
+    component : LoginComponent,
+    pathMatch : 'full'
+  },
+  {
+    path : 'sign-up',
+    component : RegistrationComponent,
+    pathMatch : 'full'
+  },
+  {
+    path : 'reset-password',
+    component : ResetPasswordComponent,
+    pathMatch : 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/ui/angular/keep/src/app/modules/login/login.module.ts
+++ src/main/ui/angular/keep/src/app/modules/login/login.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, RegistrationComponent],
+  declarations: [LoginComponent, RegistrationComponent, ResetPasswordComponent],
   imports: [
     CommonModule,
     LoginRoutingModule
