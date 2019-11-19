2268e561e4b7ef56c9a17f8768101abc80aea4ff
swanandm <swanand.mehta94@gmail.com>
2019-11-19 11:13:28 +0530

    Added :
    Removed :
    
    Modified :
    + Added routerlink into login component
    + Added login routing for registration component
    
    Signed-off-by: swanandm <swanand.mehta94@gmail.com>

--- src/main/ui/angular/keep/src/app/modules/login/login-routing.module.ts
+++ src/main/ui/angular/keep/src/app/modules/login/login-routing.module.ts
@@ -1,12 +1,21 @@
+import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{
-  path : '',
-  component : LoginComponent
-}];
+const routes: Routes = [
+  {
+    path : '',
+    component : LoginComponent,
+    pathMatch : 'full'
+  },
+  {
+    path : 'sign-up',
+    component : RegistrationComponent,
+    pathMatch : 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
