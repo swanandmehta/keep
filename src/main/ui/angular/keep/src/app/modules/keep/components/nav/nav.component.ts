4a7651b53e987ff0b078ad7ea725ce0a14e637e7
swanandm <swanandmehta94@gmail.com>
2019-12-06 11:06:44 +0530

    Added :
    Removed :
    
    Modified :
    + Nav component UI design completed
    
    Signed-off-by: swanandm <swanand.mehta94@gmail.com>

--- src/main/ui/angular/keep/src/app/modules/keep/components/nav/nav.component.ts
+++ src/main/ui/angular/keep/src/app/modules/keep/components/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { IconDefinition, faLightbulb, faClock, faPlus, faBookmark, faArchive, faTrashAlt, faCogs } from '@fortawesome/free-solid-svg-icons';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+
+  private noteIcon: IconDefinition = faLightbulb;
+  private reminderIcon: IconDefinition = faClock;
+  private addNewIcon: IconDefinition = faPlus;
+  private labelIcon: IconDefinition = faBookmark;
+  private archiveIcon: IconDefinition = faArchive;
+  private trashIcon: IconDefinition = faTrashAlt;
+  private settingsIcon: IconDefinition = faCogs;
 
   constructor() { }
 
