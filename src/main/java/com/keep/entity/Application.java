f5c20ee7f83a6ac53300ee9c66f6bdbaf0cd1001
swanandm <swanand.mehta94@gmail.com>
2020-01-12 15:40:21 +0530

    Adding entity information for appliction and application user

--- src/main/java/com/keep/entity/Application.java
+++ src/main/java/com/keep/entity/Application.java
@@ -3,10 +3,80 @@
  */
 package com.keep.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 /**
  * @author swanandm
  *
  */
+@Entity
+@Table(name="APP")
 public class Application implements IKeepEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Integer id;
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="IMG_DIR")
+	private String imgLocation;
+	
+	@Column(name="CREATED_BY")
+	private Integer creatorId;
+	
+	@OneToOne(cascade= {}, fetch=FetchType.LAZY)
+	@JoinColumn(name="CREATED_BY", insertable=false, updatable=false)
+	private User creator;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImgLocation() {
+		return imgLocation;
+	}
+
+	public void setImgLocation(String imgLocation) {
+		this.imgLocation = imgLocation;
+	}
+
+	public Integer getCreatorId() {
+		return creatorId;
+	}
+
+	public void setCreatorId(Integer creatorId) {
+		this.creatorId = creatorId;
+	}
+
+	public User getCreator() {
+		return creator;
+	}
+
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
 
 }
--- src/main/java/com/keep/entity/ApplicationUser.java
+++ src/main/java/com/keep/entity/ApplicationUser.java
@@ -3,10 +3,81 @@
  */
 package com.keep.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 /**
  * @author swanandm
  *
  */
+@Entity
+@Table(name="users_app")
 public class ApplicationUser implements IKeepEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Integer id;
+	
+	@Column(name="USER_ID")
+	private Integer userId;
+	
+	@Column(name="APP_ID")
+	private Integer appId;
+	
+	@OneToOne(cascade= {}, fetch=FetchType.LAZY)
+	@JoinColumn(name="USER_ID", insertable=false, updatable=false)
+	private User user;
+	
+	@OneToOne(cascade= {}, fetch=FetchType.LAZY)
+	@JoinColumn(name="APP_ID")
+	private Application application;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getAppId() {
+		return appId;
+	}
+
+	public void setAppId(Integer appId) {
+		this.appId = appId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Application getApplication() {
+		return application;
+	}
+
+	public void setApplication(Application application) {
+		this.application = application;
+	}
 
 }
