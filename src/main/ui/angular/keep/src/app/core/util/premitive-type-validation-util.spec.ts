cf935a01af434f42d563ed1f44357216f618bcbc
Swanand Mehta <swanandmehta94@gmail.com>
2020-02-23 15:55:43 +0530

    Remove test files since new test package will be created for unit testing

--- src/main/ui/angular/keep/src/app/core/util/premitive-type-validation-util.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PremitiveTypeValidationUtil } from './premitive-type-validation-util';
-
-describe('PremitiveTypeValidationUtil', () => {
-  it('should create an instance', () => {
-    expect(new PremitiveTypeValidationUtil()).toBeTruthy();
-  });
-});
