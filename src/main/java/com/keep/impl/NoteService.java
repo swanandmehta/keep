f06d7aed623a29858929673405dff3639f611255
Swanand Mehta <swanandmehta94@gmail.com>
2020-03-04 14:35:18 +0530

    Added functionality to remove duplicate values

--- src/main/java/com/keep/impl/NoteService.java
+++ src/main/java/com/keep/impl/NoteService.java
@@ -4,14 +4,20 @@
 package com.keep.impl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.keep.dto.NoteDto;
 import com.keep.dto.ServerNotesSearchCriteria;
+import com.keep.entity.Label;
 import com.keep.entity.Note;
 import com.keep.enums.NoteType;
+import com.keep.repository.ILabelRepository;
 import com.keep.repository.INoteRepository;
 import com.keep.services.INoteServices;
 import com.keep.transformer.NoteTransformer;
@@ -24,10 +30,12 @@ import com.keep.transformer.NoteTransformer;
 public class NoteService extends CrudService<NoteDto, Note> implements INoteServices {
 	
 	private final INoteRepository noteRepository;
+	private final ILabelRepository labelRepository;
 
-	public NoteService(INoteRepository noteRepository) {
+	public NoteService(INoteRepository noteRepository, ILabelRepository labelRepository) {
 		super(noteRepository);
 		this.noteRepository = noteRepository;
+		this.labelRepository = labelRepository;
 	}
 
 	@Override
@@ -38,6 +46,37 @@ public class NoteService extends CrudService<NoteDto, Note> implements INoteServ
 	@Override
 	public Note toEntity(NoteDto dto) {
 		return NoteTransformer.transform(dto);
+	}
+	
+	@Override
+	public Note save(Note entity) {
+		Set<Label> labelSet = entity.getLabelSet();
+		
+		Set<String> labelNameSet = labelSet.stream()
+											.map(label -> label.getName().toUpperCase())
+											.collect(Collectors.toSet());
+		
+		Set<Label> dbLabelSet = labelRepository.findByName(labelNameSet);
+		
+		labelSet.addAll(dbLabelSet);
+		
+		Map<String, Label> commonValues = labelSet.stream()
+													.collect(Collectors.toMap(
+																	label -> label.getName().toUpperCase(), 
+																	label -> label,
+																	(addedLabel, newLabel) -> {
+																		if(addedLabel.getId() != null) {
+																			return addedLabel;																			
+																		}else if(newLabel.getId() != null) {
+																			return newLabel;
+																		}
+																		return newLabel;
+																	}
+																)
+															);
+		entity.setLabelSet(new HashSet<Label>(commonValues.values()));
+		
+		return super.save(entity);
 	}
 
 	@Override
--- src/main/java/com/keep/repository/ILabelRepository.java
+++ src/main/java/com/keep/repository/ILabelRepository.java
@@ -4,7 +4,10 @@
 package com.keep.repository;
 
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.keep.entity.Label;
@@ -17,5 +20,8 @@ import com.keep.entity.Label;
 public interface ILabelRepository extends ICrudRepository<Label> {
 
 	List<Label> getByUserIdAndName(Integer userId, String label);
+
+	@Query("SELECT e FROM com.keep.entity.Label e WHERE UPPER(e.name) IN :labelNameList")
+	Set<Label> findByName(@Param("labelNameList") Set<String> labelNameSet);
 
 }
